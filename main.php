4a27433bfe9a8729ccf096b1c0e378cda646534c
josielqogit <josielqo@gmail.com>
2019-12-11 15:27:31 -0300

    Ajustes entrega

--- main.php
+++ main.php
@@ -2,109 +2,110 @@
     
     <main class="hotsite--container">
       <div class="hotsite--background">
+      <div class="hotsite--content-wrapper">
+        <section class="hotsite--title">
+          <h1>Políticas<span class="ornament"></span> comerciais<span class="ornament"></span></h1>
+        </section><!-- title -->
 
-      <section class="hotsite--title">
-        <h1>Políticas<span class="ornament"></span> comerciais<span class="ornament"></span></h1>
-      </section><!-- title -->
-
-        <section class="hotsite--frete">
-          <div class="hotsite--frete__wrapper">
-            <div class="hotsite--frete__hero">
-              <div class="hotsite--hero__title">
-                <h1>Title</h1>
+          <section class="hotsite--frete">
+            <div class="hotsite--frete__wrapper">
+              <div class="hotsite--frete__hero">
+                <div class="hotsite--hero__title">
+                  <h1>Como funciona o frete?</h1>
+                </div>
+                <div class="hotsite--hero__icon">
+                  <img src="/images-hot-3/frete.png">
+                </div>
               </div>
-              <div class="hotsite--hero__icon">
-                <img src="/images-hot-3/frete.png">
+              <div class="hotsite--frete__content">
+                <p>Pedidos acima de R$ 100 dão direito a Frete Grátis e em compras abaixo desse valor, será cobrada uma taxa de entrega de R$ 20.</p>
+                <p>Caso você utilize um vale-compra, para manter o frete gratuito seu carrinho deverá somar R$ 100 sem considerar o desconto.</p>
               </div>
             </div>
-            <div class="hotsite--frete__content">
-              <p>lorem ipsum</p>
-              <p>lorem ipsum</p>
+          </section><!-- frete -->
+
+          <section class="hotsite--prazo">
+            <div class="hotsite--prazo__wrapper">
+              <div class="hotsite--prazo__hero">
+                <div class="hotsite--hero__title">
+                  <h1>Prazo de entrega</h1>
+                </div>
+                <div class="hotsite--hero__icon">
+                  <img src="/images-hot-3/prazo.png">
+                </div>
+              </div>
+              <div class="hotsite--prazo__content">
+                <p>O prazo ao lado é exibido em dias úteis, você poderá iniciar a contagem a partir do momento em que seu pedido for faturado.</p>
+                <p>Fique tranquilo, você receberá um email de aviso quando isso acontecer.</p>
+              </div>
             </div>
+          </section><!-- prazo -->
+        </div><!-- background -->
+
+        <section class="hotsite--cta">
+          <div class="hotsite--cta__wrapper">
+            <button>cta</button>
           </div>
-        </section><!-- frete -->
+        </section><!-- cta -->
 
-        <section class="hotsite--prazo">
-          <div class="hotsite--prazo__wrapper">
-            <div class="hotsite--prazo__hero">
-              <div class="hotsite--hero__title">
-                <h1>Title</h1>
-              </div>
-              <div class="hotsite--hero__icon">
-                <img src="/images-hot-3/prazo.png">
-              </div>
-            </div>
-            <div class="hotsite--prazo__content">
-              <p>lorem ipsum</p>
-              <p>lorem ipsum</p>
-            </div>
-          </div>
-        </section><!-- prazo -->
-      </div><!-- background -->
+        <section class="hotsite--pagamento">
+          <div class="hotsite--pagamento__wrapper">
+            <div class="hotsite--boletos-1">
+              <div class="hotsite--boletos-1__wrapper">
+                <div class="hotsite--pagamento__hero">
+                  <div class="hotsite--pagamento__hero-icon">
+                    <img src="/images-hot-3/boleto.png">
+                  </div>
+                  <div class="hotsite--pagamento__hero-title">
+                    <h1>title</h1>
+                  </div>
+                </div>
+                <div class="hotsite--pagamento__content">
+                  <p>p-1</p>
+                  <p>p-2</p>
+                  <p>p-3</p>
+                </div>
+              </div><!-- boletos-1 wrapper -->
+            </div><!-- boletos-1  -->
 
-      <section class="hotsite--cta">
-        <div class="hotsite--cta__wrapper">
-          <button>cta</button>
-        </div>
-      </section><!-- cta -->
+            <div class="hotsite--boletos-2">
+              <div class="hotsite--boletos-2__wrapper">
+                <div class="hotsite--pagamento__hero">
+                  <div class="hotsite--pagamento__hero-icon">
+                    <img src="/images-hot-3/boleto.png">
+                  </div>
+                  <div class="hotsite--pagamento__hero-title">
+                    <h1>title</h1>
+                  </div>
+                </div>
+                <div class="hotsite--pagamento__content">
+                  <p>p-1</p>
+                  <p>p-2</p>
+                  <p>p-3</p>
+                </div>
+              </div><!-- boletos-2 wrapper -->
+            </div><!-- boletos-2  -->
 
-      <section class="hotsite--pagamento">
-        <div class="hotsite--pagamento__wrapper">
-          <div class="hotsite--boletos-1">
-            <div class="hotsite--boletos-1__wrapper">
-              <div class="hotsite--pagamento__hero">
-                <div class="hotsite--pagamento__hero-icon">
-                  <img src="/images-hot-3/boleto.png">
+            <div class="hotsite--credit-card">
+              <div class="hotsite--credit-card__wrapper">
+                <div class="hotsite--pagamento__hero">
+                  <div class="hotsite--pagamento__hero-icon">
+                    <img src="/images-hot-3/card.png">
+                  </div>
+                  <div class="hotsite--pagamento__hero-title">
+                    <h1>title</h1>
+                  </div>
                 </div>
-                <div class="hotsite--pagamento__hero-title">
-                  <h1>title</h1>
+                <div class="hotsite--pagamento__content">
+                  <p>p-1</p>
+                  <p>p-2</p>
+                  <p>p-3</p>
                 </div>
-              </div>
-              <div class="hotsite--pagamento__content">
-                <p>p-1</p>
-                <p>p-2</p>
-                <p>p-3</p>
-              </div>
-            </div><!-- boletos-1 wrapper -->
-          </div><!-- boletos-1  -->
+              </div><!-- credit-card wrapper -->
+            </div><!-- credit-card  -->
 
-          <div class="hotsite--boletos-2">
-            <div class="hotsite--boletos-2__wrapper">
-              <div class="hotsite--pagamento__hero">
-                <div class="hotsite--pagamento__hero-icon">
-                  <img src="/images-hot-3/boleto.png">
-                </div>
-                <div class="hotsite--pagamento__hero-title">
-                  <h1>title</h1>
-                </div>
-              </div>
-              <div class="hotsite--pagamento__content">
-                <p>p-1</p>
-                <p>p-2</p>
-                <p>p-3</p>
-              </div>
-            </div><!-- boletos-2 wrapper -->
-          </div><!-- boletos-2  -->
-
-          <div class="hotsite--credit-card">
-            <div class="hotsite--credit-card__wrapper">
-              <div class="hotsite--pagamento__hero">
-                <div class="hotsite--pagamento__hero-icon">
-                  <img src="/images-hot-3/card.png">
-                </div>
-                <div class="hotsite--pagamento__hero-title">
-                  <h1>title</h1>
-                </div>
-              </div>
-              <div class="hotsite--pagamento__content">
-                <p>p-1</p>
-                <p>p-2</p>
-                <p>p-3</p>
-              </div>
-            </div><!-- credit-card wrapper -->
-          </div><!-- credit-card  -->
-
-        </div><!-- pagamento -->
+          </div><!-- content wrapper -->
+        </div><!-- background -->
       </section><!-- pagamento -->
 
     </main>
